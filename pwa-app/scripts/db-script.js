3ac6dc05ddcf029489fd22feb00de4be7045a1bb
lukasz <lukasz55szary@gmail.com>
2025-05-12 19:11:52 +0200

    initial commit

---
+++ pwa-app/scripts/db-script.js
@@ -0,0 +1,42 @@
+const NewsDB = {
+    addToDB: null,
+    getAll: null,
+    ready: false
+};
+
+const dbName = 'NewsDB';
+const dbVersion = 2;
+const request = indexedDB.open(dbName, dbVersion);
+
+const newsObjectStoreKey = 'News';
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    if (!db.objectStoreNames.contains(newsObjectStoreKey)) {
+        db.createObjectStore(newsObjectStoreKey, {keyPath: 'id', autoIncrement: true });
+        console.log("db.createObjectStore: DONE");
+    }
+};
+
+request.onsuccess = (event) => {
+    const db = event.target.result;
+    NewsDB.addToDB = (news) => {
+        console.log(`adding to db${news}`);
+        const transactionType = 'readwrite';
+        const store = db.transaction(newsObjectStoreKey, transactionType).objectStore(newsObjectStoreKey);
+        store.add(news);        
+    }
+
+    NewsDB.getAll = () => {
+        return new Promise((resolve, reject) => {
+        const transactionType = 'readonly';
+        const transaction = db.transaction(newsObjectStoreKey, transactionType);
+        const store = transaction.objectStore(newsObjectStoreKey);
+        const request = store.getAll();
+            
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = (err) => reject(err);
+        });
+    }    
+    NewsDB.ready = true;
+     console.log("Database initialized");
+};
---
+++ pwa-app/scripts/favourites-script.js
@@ -0,0 +1,34 @@
+const container = document.getElementById("container");
+
+async function loadAndDisplayFavorites() {
+    try {
+        await new Promise(resolve => {
+            const checkReady = () => {
+                if (NewsDB.ready) resolve();
+                else setTimeout(checkReady, 100);
+            };
+            checkReady();
+        });
+        
+        const favNews = await NewsDB.getAll();
+        printArticles(favNews);
+    } catch (error) {
+        console.error("Error loading favorites:", error);
+    }
+}
+
+const printArticles = (articles) => {
+    articles.forEach((item) => {
+        console.log(item);
+    const div = document.createElement("div");
+    div.classList.add('article');
+    div.innerHTML = `
+     <h3>${item.title}</h3>
+     <p>${item.description}</p>
+     <a href="${item.url}">${item.url}</a>
+    `;
+    container.appendChild(div);
+    });
+}
+
+loadAndDisplayFavorites();
